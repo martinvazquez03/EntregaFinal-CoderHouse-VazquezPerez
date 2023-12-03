85f056c90c85c2ef708d81192b1d70e085121f9f
Martin Vazquez Perez <martinvp2003@outlook.com.ar>
2023-12-02 21:20:58 -0300

    proyecto iniciado

--- project/Cliente/models.py
+++ project/Cliente/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length = 100, unique = False)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    fecha_nacimiento = models.DateField(null = True, blank = True)
+    pais_origen = models.ForeignKey(Pais, on_delete = models.SET_NULL, null = True, blank = True)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    
+
+
--- project/config/settings.py
+++ project/config/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'America/Argentina/BuenosAires'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
